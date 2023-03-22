13eff5f0a149f3e9e354893f89dd7d1f824522fe
Jake Gealer <jake@gealer.email>
2023-03-22 20:34:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 style="text-align: center">contenttruck-js</h1>
+
+TODO
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// contenttruck-js. Copyright (C) Web Scale Software Ltd 2023.
+// This code is licensed under the MIT license. See the LICENSE file for details.
+
+console.log(1);
